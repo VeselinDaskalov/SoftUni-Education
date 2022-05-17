24269a9648e74a4ba0e395323a9526a55350141f
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-17 22:50:50 +0300

    Add files via upload

---
+++ Programming Basic C#/Conditional Statements - Lab/ExelantResult/ExelantResult.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExelantResult
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+
+            if (a >= 5.50 && a <= 6)
+            {
+                Console.WriteLine("Excellent!");
+            }
+           
+        }
+    }
+}
