3166f6056d0ab199edc0738e649abecf809dd1ba
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-18 09:34:09 +0300

    Add files via upload

---
+++ Programming Basic C#/Conditional Statements - Exercise/08. Lunch Break/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LunchBreak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            int episode = int.Parse(Console.ReadLine());
+            int breakTime = int.Parse(Console.ReadLine());
+
+            double lunch = breakTime / 8.0;
+            double rest = breakTime / 4.0;
+            double time = breakTime - lunch - rest;
+
+            
+
+
+            if (time >= episode)
+            {
+                double resultTime = Math.Ceiling(time - episode);
+                Console.WriteLine($"You have enough time to watch {name} and left with {resultTime} minutes free time.");
+            }
+            else
+            {
+                double neededTime = Math.Ceiling(episode - time);
+                Console.WriteLine($"You don't have enough time to watch {name}, you need {neededTime} more minutes.");
+            }
+        }
+    }
+}
