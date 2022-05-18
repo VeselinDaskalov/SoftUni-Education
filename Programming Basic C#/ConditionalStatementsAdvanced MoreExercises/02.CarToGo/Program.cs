d9201e6b88022064ca4665c46d3233d69985c1ac
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-18 09:45:30 +0300

    Add files via upload

---
+++ Programming Basic C#/ConditionalStatementsAdvanced MoreExercises/02.CarToGo/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarToGo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string season = Console.ReadLine();
+            double price = 0.00;
+            string clasCar = "";
+            string typeCar = "";
+
+            if (budget <= 100)
+            {
+                clasCar = "Economy class";
+                if (season == "Summer")
+                {
+                    typeCar = "Cabrio";
+                    price = budget * 0.35;
+
+                }
+                else if (season == "Winter")
+                {
+                    typeCar = "Jeep";
+                    price = budget * 0.65;
+                }
+
+            }
+            else if (budget > 100 && budget <= 500)
+            {
+                clasCar = "Compact class";
+                if (season == "Summer")
+                {
+                    typeCar = "Cabrio";
+                    price = budget * 0.45;
+                }
+                else if (season == "Winter")
+                {
+                    typeCar = "Jeep";
+                    price = budget * 0.80;
+                }
+            }
+            else if (budget > 500)
+            {
+                clasCar = "Luxury class";
+                typeCar = "Jeep";
+                price = budget * 0.90;
+            }
+            Console.WriteLine($"{clasCar}");
+            Console.WriteLine($"{typeCar} - {price:f2}");
+
+        }
+    }
+}
