4a47ee8c599091a7db43680ced4f4aceeaf1b56b
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-19 10:42:09 +0300

    Add files via upload

---
+++ Programming Basic C#/Conditional Statements Advanced - Lab/07.Working Hours/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkingHours
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            string day = Console.ReadLine();
+
+            if (hours >= 10 && hours <= 18)
+            {
+                switch (day)
+                {
+                    case "Monday":
+                    case "Tuesday":
+                    case "Wednesday":
+                    case "Thursday":
+                    case "Friday":
+                    case "Saturday":
+                        Console.WriteLine("open");
+                        break;
+                    case "Sunday":
+                        Console.WriteLine("closed");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("closed");
+            }
+        }
+    }
+}
