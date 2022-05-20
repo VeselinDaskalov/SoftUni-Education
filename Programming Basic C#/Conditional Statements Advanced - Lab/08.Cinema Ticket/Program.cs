cd6645c2679950b8cd0198a9729e8ff952e4a81a
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-20 11:44:46 +0300

    Add files via upload

---
+++ Programming Basic C#/Conditional Statements Advanced - Lab/08.Cinema Ticket/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CinemaTicket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+
+            if (day == "Monday" || day =="Tuesday"|| day =="Friday")
+            {
+                Console.WriteLine(12);
+            }
+            else if (day == "Wednesday"|| day== "Thursday")
+            {
+                Console.WriteLine(14);
+            }    
+            else
+            {
+                Console.WriteLine(16);
+            }
+        }
+    }
+}
