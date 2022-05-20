c792efcc07e3682619177d6f06c6b821c9a0a029
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-20 11:46:16 +0300

    Add files via upload

---
+++ Programming Basic C#/Conditional Statements Advanced - Lab/12. Trade Commissions/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TradeCommissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = Console.ReadLine();
+            double sales = double.Parse(Console.ReadLine());
+            double comission = 0.0;
+            bool error = false;
+            if (city == "Sofia")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    comission = sales * 0.05;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    comission = sales * 0.07;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    comission = sales * 0.08;
+                }
+                else if (sales > 10000)
+                {
+                    comission = sales * 0.12;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    error = true;
+                }
+            }
+            else if (city == "Varna")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    comission = sales * 0.045;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    comission = sales * 0.075;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    comission = sales * 0.1;
+                }
+                else if (sales > 10000)
+                {
+                    comission = sales * 0.13;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    error = true;
+                }
+            }
+            else if (city == "Plovdiv")
+            {
+                if (sales >= 0 && sales <= 500)
+                {
+                    comission = sales * 0.055;
+                }
+                else if (sales > 500 && sales <= 1000)
+                {
+                    comission = sales * 0.08;
+                }
+                else if (sales > 1000 && sales <= 10000)
+                {
+                    comission = sales * 0.12;
+                }
+                else if (sales > 10000)
+                {
+                    comission = sales * 0.145;
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    error = true;
+                }
+            }
+
+            else
+            {
+                Console.WriteLine("error");
+                error = true;
+            } 
+            if (error == false)
+            {
+                Console.WriteLine($"{comission:f2}");
+            }
+           
+            
+        }
+            
+    }
+}
