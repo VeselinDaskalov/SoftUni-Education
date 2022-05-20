c53b64f8415cb073ae41fc091aeb4ba2db980633
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-20 11:45:36 +0300

    Add files via upload

---
+++ Programming Basic C#/Conditional Statements Advanced - Lab/10. Invalid Number/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvalidNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            if ((number < 100 || number > 200) && number != 0) Console.WriteLine("invalid");
+        }
+    }
+}
