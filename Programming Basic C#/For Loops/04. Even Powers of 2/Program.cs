f4fd310d1df3441631f9f5ba37a82fe0fb713d8f
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-21 15:52:10 +0300

    Add files via upload

---
+++ Programming Basic C#/For Loops/04. Even Powers of 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenPowersOf2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 1; 
+
+            for (int cnt = 0; cnt <= n; cnt += 2)
+            {
+                Console.WriteLine(num);
+                num *= 2*2;
+            }
+        }
+    }
+}
