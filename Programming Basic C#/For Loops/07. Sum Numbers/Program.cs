c0824f000d3aa99ea02cd620c8d8bd9f2874d83e
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-22 11:33:30 +0300

    Add files via upload

---
+++ Programming Basic C#/For Loops/07. Sum Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int totalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                totalSum += number;
+            }
+
+            Console.WriteLine(totalSum);
+        }
+    }
+}
