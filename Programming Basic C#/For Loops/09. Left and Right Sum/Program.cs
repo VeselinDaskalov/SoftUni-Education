c33ee9a5fdf09978bc79cd5f3e743dbc91a7fe35
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-23 11:52:08 +0300

    Add files via upload

---
+++ Programming Basic C#/For Loops/09. Left and Right Sum/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeftAndRightSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numsCnt = int.Parse(Console.ReadLine());
+            int firstSum = 0;
+            for(int cnt = 0; cnt < numsCnt; cnt++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                firstSum +=number;
+            }
+            int secondSum = 0;
+            for(int cnt = 0; cnt < numsCnt; cnt++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                secondSum += number;
+            }
+            if (firstSum == secondSum)
+            {
+                Console.WriteLine($"Yes, sum = {firstSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(firstSum - secondSum)}");
+            }
+        }
+    }
+}
