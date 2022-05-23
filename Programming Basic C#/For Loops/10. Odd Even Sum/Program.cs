5b0ba8827b4f59623fa59664e20242694236e840
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-23 11:52:24 +0300

    Add files via upload

---
+++ Programming Basic C#/For Loops/10. Odd Even Sum/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+
+                if (i % 2 == 0) // не n, а четни позиции 
+                {
+                    evenSum += number;
+                }
+                else if (i % 2 != 0) // не n, а четни позиции 
+                {
+                    oddSum += number;
+                }
+            }
+
+
+            if (evenSum == oddSum)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {evenSum}");
+            }
+            else if (evenSum != oddSum)
+            {
+                int finsum = Math.Abs(oddSum - evenSum);
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {finsum}");
+            }
+        }
+    }
+}
