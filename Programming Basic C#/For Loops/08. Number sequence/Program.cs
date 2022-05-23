e42580cf9b3ba2dbd46fd33ad582f74ff9fdb135
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-23 11:51:49 +0300

    Add files via upload

---
+++ Programming Basic C#/For Loops/08. Number sequence/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberSequence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using System;
+
+namespace NumberSequence
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                int countNumbers = int.Parse(Console.ReadLine());
+
+                int biggestNum = int.MinValue;
+                int smallestNum = int.MaxValue;
+
+                for (int i = 0; i < countNumbers; i++)
+                {
+                    int number = int.Parse(Console.ReadLine());
+                    if (biggestNum < number)
+                    {
+                        biggestNum = number;
+                    }
+                    if (smallestNum > number)
+                    {
+                        smallestNum = number;
+                    }
+                }
+                Console.WriteLine($"Max number: {biggestNum}");
+                Console.WriteLine($"Min number: {smallestNum}");
+            }
+        }
+    }
+}
+    }
+}
