0bab0aadcbd7d60430e3971ed9b772ef8fcfb958
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-27 10:12:04 +0300

    Add files via upload

---
+++ Programming Basic C#/For Loop - Exercise/04.CleverLily/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverLily
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());    
+            double washingMachine = double.Parse(Console.ReadLine());
+            int toyPrice = int.Parse(Console.ReadLine());
+
+            //когато i  е четно => i % 2 == 0 => i*5-1
+            //когато i  е нечетно => i % 2 != 0 => получава играчка toyPrice
+            double money = 0;
+
+            for (int i = 1; i <= age; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    money = money + i * 5 - 1;
+                }
+                else
+                {
+                    money = money + toyPrice;
+                }
+            }
+            if (money >= washingMachine)
+            {
+                Console.WriteLine($"Yes! {money - washingMachine:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"No! {washingMachine - money:f2}");
+            }
+
+
+        }
+
+    }
+}
