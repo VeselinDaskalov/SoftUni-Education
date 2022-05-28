bc4bdae1a78a811731b57789f85e7bcbdcdcfe81
VeselinDaskalov <97159720+VeselinDaskalov@users.noreply.github.com>
2022-05-28 10:35:25 +0300

    Add files via upload

---
+++ Programming Basic C#/For Loop - Exercise/05.Salary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine()); // брой отворени табове
+            int salary = int.Parse(Console.ReadLine());
+
+            int penalty = 0;
+
+            for (int i = 1; i <= tabs; i++)
+            {
+                string site = Console.ReadLine();
+
+                if (site == "Facebook")
+                {
+                    penalty += 150; 
+                }
+                else if (site == "Instagram")
+                {
+                    penalty += 100;
+                }
+                else if (site == "Reddit")
+                {
+                    penalty += 50;
+                }
+                if (penalty >= salary)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+            if (penalty < salary)
+            {
+                Console.WriteLine(salary - penalty);
+            }
+        }
+    }
+}
