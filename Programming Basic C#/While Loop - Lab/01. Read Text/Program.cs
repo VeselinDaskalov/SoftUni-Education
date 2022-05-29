1aad8916fe49cd58c914250845dffcabd1472969
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-05-29 18:26:46 +0300

    While Loop - Lab added, solution problem 01. Read Text

---
+++ Programming Basic C#/While Loop - Lab/01. Read Text/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01ReadText
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "Stop")
+            {
+                Console.WriteLine(input);
+                input = Console.ReadLine();
+
+            }
+        }
+    }
+}
