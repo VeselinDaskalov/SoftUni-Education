937ef8f15be677a6554036d3f534447b4125ac83
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-05-30 22:34:29 +0300

    Solution problem 03.Sum Numbers added

---
+++ Programming Basic C#/While Loop - Lab/03. Sum Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03SumNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (sum < number)
+            {
+                sum += int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(sum);
+
+        }
+    }
+}
