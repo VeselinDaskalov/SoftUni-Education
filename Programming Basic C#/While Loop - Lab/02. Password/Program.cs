da1f5b7dbc2a61daf8c2398a4b23c258c4afc5f9
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-05-30 22:32:29 +0300

    Solution problem 02 Password added

---
+++ Programming Basic C#/While Loop - Lab/02. Password/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Password
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Account Credential
+            string userName = Console.ReadLine();
+            string password = Console.ReadLine();
+
+
+
+            int sumPass = 0;
+
+
+
+            string passwordAttempt = Console.ReadLine();
+
+            while(passwordAttempt != password)
+            {
+                sumPass++;               
+                
+                if (sumPass == 3)
+                {
+                    break;
+                }
+                passwordAttempt = Console.ReadLine();
+
+            }
+            if (password == passwordAttempt)
+            {
+                Console.WriteLine($"Welcome {userName}!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong user name or password!");
+            }
+            
+
+
+        }
+    }
+}
