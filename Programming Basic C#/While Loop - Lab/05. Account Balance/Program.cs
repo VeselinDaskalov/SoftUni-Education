7f30780c21140574ceddbfb59af715739e40f685
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-05-31 09:24:49 +0300

    Solution problem 05.Account Balance added

---
+++ Programming Basic C#/While Loop - Lab/05. Account Balance/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05AccountBalance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            double account = 0;
+
+            while (input != "NoMoreMoney")
+            {   
+                double amount = double.Parse(input);
+                if (amount < 0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                account += amount;
+                Console.WriteLine($"Increase: {amount:f2}");
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {account:f2}");
+        }
+    }
+}
