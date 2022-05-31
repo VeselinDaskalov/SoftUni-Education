e975a2f2e260682c61217129ba0fc5b07b2c24af
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-05-31 12:04:06 +0300

    Solution problem 06. MAx Number added

---
+++ Programming Basic C#/While Loop - Lab/06. Max Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06MaxNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int maxNumber = int.MinValue;
+
+            while (command != "Stop")
+            {
+                int n = int.Parse(command);
+                if (n > maxNumber)
+                {
+                    maxNumber = n;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(maxNumber);
+        }
+    }
+}
