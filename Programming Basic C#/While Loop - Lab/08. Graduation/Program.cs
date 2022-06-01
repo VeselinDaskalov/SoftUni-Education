3b61940f53b5b8551c4c27d893969745c3c75870
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-01 12:33:23 +0300

    Solution problem 08.Graduation added

---
+++ Programming Basic C#/While Loop - Lab/08. Graduation/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08Graduation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name =  Console.ReadLine();
+            int grade = 1;// the current grade of the student
+            double sum = 0;
+            int count = 0;
+
+            while (grade <= 12)
+            {
+                double yearlyGrade = double.Parse(Console.ReadLine());
+
+                if (yearlyGrade < 4)
+                {
+                    if (count == 1)
+                    {
+                        break;
+                    }
+                    count++;
+                    continue;
+                }
+
+                sum += yearlyGrade;
+                grade++;
+            }
+            double averageGrade = sum / 12;
+
+            if (grade > 12)
+            {
+                Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {grade} grade");
+            }
+            
+
+        }
+    }
+}
