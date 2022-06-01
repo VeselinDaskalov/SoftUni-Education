7e84d1285cf7ce4fc738fc3fea58e96aa326d2d4
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-01 09:34:17 +0300

    Solution 07.Min Number added

---
+++ Programming Basic C#/While Loop - Lab/07. Min Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07MinNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int minNumber = int.MaxValue;
+
+            while (command != "Stop")
+            {
+                int n = int.Parse(command);
+                if (n < minNumber)
+                {
+                    minNumber = n;
+                }
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(minNumber);
+        }
+    }
+}
