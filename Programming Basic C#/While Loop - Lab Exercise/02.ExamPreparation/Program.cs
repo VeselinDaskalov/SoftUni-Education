e9c617e757a0e9c1d6106c9c57b651e20303c10e
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-02 10:36:32 +0300

    Solution problem 02.Exam Preparation added

---
+++ Programming Basic C#/While Loop - Lab Exercise/02.ExamPreparation/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02ExamPreparation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int failedTimes = int.Parse(Console.ReadLine());
+
+            double evaluationSum = 0;
+            int evaluationCount = 0;
+            string lastExercise = "";
+            int evaluation = 0;
+            int failedCount = 0;
+
+            string input = Console.ReadLine();
+
+            while (input != "Enough")
+            {
+                lastExercise = input;
+
+                evaluation = int.Parse(Console.ReadLine());
+
+                evaluationSum += evaluation;
+                evaluationCount++;
+
+                if (evaluation <= 4)
+                {
+                    failedCount++;
+
+                    if (failedCount == failedTimes)
+                    {
+                        Console.WriteLine($" You need a break, {failedCount} poor grades.");
+                        break;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            
+
+            if (failedCount != failedTimes)
+            {
+                double evaluationAverage = evaluationSum / evaluationCount;
+                Console.WriteLine($"Average score: {evaluationAverage:f2}");
+                Console.WriteLine($"Number of problems: {evaluationCount}");
+                Console.WriteLine($"Last problem: {lastExercise}");
+            }
+            
+        }
+    }
+}
