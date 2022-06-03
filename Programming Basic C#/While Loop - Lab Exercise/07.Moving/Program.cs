df132aba497c4b1e6dc8ef85072d4802c62f28a0
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-03 09:15:44 +0300

    Solution problem 07.Moving added

---
+++ Programming Basic C#/While Loop - Lab Exercise/07.Moving/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Moving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            int freespace = width * length * height;
+            int sum = 0;
+            string command = Console.ReadLine();
+
+            while (command != "Done")
+            {
+                int currentCubic = int.Parse(command);
+                sum += currentCubic;
+                if (freespace < sum)
+                {
+                    int neededCubics = sum - freespace;
+                    Console.WriteLine($"No more free space! You need {neededCubics} Cubic meters more.");
+                    break;
+                }
+                command = Console.ReadLine();
+            }
+            if (command == "Done")
+            {
+                int freeCubics = freespace - sum;
+                Console.WriteLine($"{freeCubics} Cubic meters left.");
+            }
+        }
+    }
+}
