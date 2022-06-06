e064172bf636954b8028d3ccefc89fb2666fe41c
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-06 10:28:03 +0300

    Nested Loop - Lab/02MultiplicationTable/Program.cs solution added

---
+++ Programming Basic C#/Nested Loop - Lab/02MultiplicationTable/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    int product = x * y;
+                    Console.WriteLine($"{x} * {y} = {product}");
+                }
+            }
+        }
+    }
+}
