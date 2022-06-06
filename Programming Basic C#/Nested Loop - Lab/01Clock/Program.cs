79b7040a2030f0ab63467a9ee9f2c557800d09b0
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-06 10:26:51 +0300

    Nested Loop - Lab/01Clock/Program.cs Solution added

---
+++ Programming Basic C#/Nested Loop - Lab/01Clock/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01Clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour <= 23; hour++)
+            {
+                for (int min = 0; min <= 59; min++)
+                {
+                    Console.WriteLine($"{hour}:{min}");                    
+                }
+            }
+        }
+    }
+}
