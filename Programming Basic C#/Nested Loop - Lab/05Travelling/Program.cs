9f95e4ebff4d5f240708724512d1e64520990089
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-08 07:16:27 +0300

    Nested Loop - Lab/05Travelling/Program.cs solution added

---
+++ Programming Basic C#/Nested Loop - Lab/05Travelling/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05Travelling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string destination = Console.ReadLine();
+                if (destination == "End")
+                    break;
+
+                double expences = double.Parse(Console.ReadLine()), savings = 0;
+
+                while (savings < expences)
+                {
+                    double money = double.Parse(Console.ReadLine());
+                    savings += money;
+                }
+
+                Console.WriteLine($"Going to {destination}!");
+        }   }
+    }
+}
