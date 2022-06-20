9a93c48f9e124d3fa6e0827ff391d8d446c57e2a
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-20 08:32:19 +0300

    Basic Syntax, Conditional Statements and Loops/11. Multiplication Table 2.0/Program.cs solution added

---
+++ C# Fudamentals/Basic Syntax, Conditional Statements and Loops/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11.Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int theInteger = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{theInteger} X {times} = {theInteger * times}");
+                times++;
+            }while(times <=10);
+            
+        }
+    }
+}
