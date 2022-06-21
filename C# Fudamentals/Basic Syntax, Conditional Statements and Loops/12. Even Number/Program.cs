ca54514eb66922b4a5f69401655b0704c909b425
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-21 10:56:40 +0300

    Basic Syntax, Conditional Statements and Loops/12. Even Number/Program.cs solution added

---
+++ C# Fudamentals/Basic Syntax, Conditional Statements and Loops/12. Even Number/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Even_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            while (number % 2 != 0)
+            {
+                Console.WriteLine("Please write an even number.");
+                number = int.Parse(Console.ReadLine());
+            }
+            if (number % 2 == 0)
+            {
+                number = Math.Abs(number);
+                Console.WriteLine($"The number is: {number}");
+            }
+        }
+    }
+}
