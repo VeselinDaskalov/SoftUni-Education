b36891f894301bfae82756377cdfbfae9d1f24b6
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-27 09:40:52 +0300

    Lab Data Types and Variables/1. Convert Meters to Kilometers/Program.cs solution added

---
+++ C# Fudamentals/Lab Data Types and Variables/1. Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Convert_Meters_to_Kilometers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int meters = int.Parse(Console.ReadLine());
+
+            double kilometers = meters / 1000d;
+
+            Console.WriteLine($"{kilometers:f2}");
+        }
+    }
+}
