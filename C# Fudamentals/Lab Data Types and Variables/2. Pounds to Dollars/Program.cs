c75ded1a961e23f150631ef000442090ed20102d
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-28 10:50:56 +0300

    Lab Data Types and Variables/2. Pounds to Dollars/Program.cs  solution added

---
+++ C# Fudamentals/Lab Data Types and Variables/2. Pounds to Dollars/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Pounds_to_Dollars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double pounds = double.Parse(Console.ReadLine());
+            double dolars = pounds * 1.31;
+
+            Console.WriteLine($"{dolars:f3}");
+        }
+    }
+}
