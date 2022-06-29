7f4e25c767e74d390a12580aa44a16be8b3fc142
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-29 12:31:18 +0300

    Lab Data Types and Variables/6. Reversed Chars/Program.cs solution added

---
+++ C# Fudamentals/Lab Data Types and Variables/6. Reversed Chars/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Reversed_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char  index1 = char.Parse(Console.ReadLine());
+            char  index2 = char.Parse(Console.ReadLine());
+            char  index3 = char.Parse(Console.ReadLine());
+
+            Console.Write($"{index3} {index2} {index1}");
+
+        }
+    }
+}
