34623f76e8cd7786ee07d348ebf8c2f66311a1a2
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-29 12:30:13 +0300

    Lab Data Types and Variables/4. Centuries to Minutes/Program.cs solution added

---
+++ C# Fudamentals/Lab Data Types and Variables/4. Centuries to Minutes/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Centuries_to_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int centures = int.Parse(Console.ReadLine());
+            int years = centures * 100;
+            int days =  (int)(years * 365.2422);
+            long hours = days * 24;
+            long minutes = hours * 60;
+
+            Console.WriteLine("{0} centuries = {1} years = {2} days = {3} hours = {4} minutes", centures, years, days, hours, minutes);
+        }
+    }
+}
