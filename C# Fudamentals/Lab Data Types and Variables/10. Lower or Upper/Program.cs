6c5c07a37a735748a6932a8fc519710cf69793fc
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-30 09:03:34 +0300

    Lab Data Types and Variables/10. Lower or Upper/Program.cs solution added

---
+++ C# Fudamentals/Lab Data Types and Variables/10. Lower or Upper/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Lower_or_Upper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char ch = char.Parse(Console.ReadLine());
+
+            if (Char.IsUpper(ch))
+            {
+                Console.WriteLine("upper-case");
+            }
+            else if(Char.IsLower(ch))
+            {
+                Console.WriteLine("lower-case");
+            }
+        }
+    }
+}
