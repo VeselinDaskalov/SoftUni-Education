7563e003aee20192b27ac3f00e854f77bddc9d88
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-06-30 09:02:31 +0300

    Lab Data Types and Variables/9. Chars to String/Program.cs solution added

---
+++ C# Fudamentals/Lab Data Types and Variables/9. Chars to String/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Chars_to_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char index1 = char.Parse(Console.ReadLine());
+            char index2 = char.Parse(Console.ReadLine());
+            char index3 = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{index1}{index2}{index3}");
+        }
+    }
+}
