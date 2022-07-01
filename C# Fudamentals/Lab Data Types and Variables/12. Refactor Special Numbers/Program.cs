cf7e31dad5ff054a87b0ef6be6ac5a10b9ba0fb7
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-01 08:25:57 +0300

    Lab Data Types and Variables/12. Refactor Special Numbers/Program.cs solution added

---
+++ C# Fudamentals/Lab Data Types and Variables/12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12.Refactor_Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int digit = 0;
+            bool isSpecialNum = false;
+            for (int ch = 1; ch <= num; ch++)
+            {
+                digit = ch;
+                while (ch > 0)
+                {
+                    sum += ch % 10;
+                    ch = ch / 10;
+                }
+                isSpecialNum = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine("{0} -> {1}", digit, isSpecialNum);
+                sum = 0;
+                ch = digit;
+            }
+
+        }
+    }
+}
