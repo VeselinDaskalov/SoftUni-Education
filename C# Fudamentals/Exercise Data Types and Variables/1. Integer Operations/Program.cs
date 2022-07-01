13d5d67028a8bb1f6efe9a7d58e3e90fd4041830
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-01 08:45:00 +0300

    Exercise Data Types and Variables/1. Integer Operations/Program.cs solution added

---
+++ C# Fudamentals/Exercise Data Types and Variables/1. Integer Operations/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Integer_Operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
