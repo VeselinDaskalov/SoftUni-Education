9eee41bbb41743c4be6f89d11d24787f7c6a9ff6
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-02 08:34:58 +0300

    Exercise Data Types and Variables/4. Sum of Chars/Program.cs solution added

---
+++ C# Fudamentals/Exercise Data Types and Variables/4. Sum of Chars/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Sum_of_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int number = int.Parse(Console.ReadLine());
+            //long sum = 0;
+
+            //for (int i = 0; i < number; i++)
+            //{
+            //    char ch = char.Parse(Console.ReadLine());
+            //    sum += (int)ch;
+            //}
+
+            //Console.WriteLine($"The sum equals: {sum}");
+
+            int number = int.Parse(Console.ReadLine());
+            int finalSum = 0;
+            for (int i = 0; i < number; i++)
+            {
+                char digit = char.Parse(Console.ReadLine());
+                finalSum += (int)digit;
+            }
+
+            Console.WriteLine($"The sum equals: {finalSum}");
+        }
+    }
+}
