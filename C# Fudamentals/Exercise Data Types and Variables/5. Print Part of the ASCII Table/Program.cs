b82a14e32f46975c10dace092ca2df7c81b82be9
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-03 09:48:23 +0300

    Exercise Data Types and Variables/5. Print Part of the ASCII Table/Program.cs solution added

---
+++ C# Fudamentals/Exercise Data Types and Variables/5. Print Part of the ASCII Table/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5.Print_Part_of_the_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndex = int.Parse(Console.ReadLine());
+            int endIndex = int.Parse(Console.ReadLine());
+             
+            for (int i = startIndex; i <= endIndex; i++)
+            {
+                string asciiChar = ((char)i).ToString();
+                Console.Write(asciiChar + " ");
+            }
+        }
+    }
+}
