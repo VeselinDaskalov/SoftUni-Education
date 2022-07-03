351445409cdc4abd36baffda19efeda4206186d0
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-03 09:49:27 +0300

    Exercise Data Types and Variables/6. Triples of Latin Letters/Program.cs solution added

---
+++ C# Fudamentals/Exercise Data Types and Variables/6. Triples of Latin Letters/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6.Triples_of_Latin_Letters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (char i = 'a'; i < 'a' + number; i++)
+            {
+                for (char j = 'a'; j < 'a' + number; j++)
+                {
+                    for (char k = 'a'; k < 'a' + number; k++)
+                    {
+                        Console.WriteLine($"{i}{j}{k}");
+                    }
+                } 
+            } 
+        }
+    }
+}
