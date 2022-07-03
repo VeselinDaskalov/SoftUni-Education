df5169c883755ba524bbeafeb68d137b74e223c0
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-03 09:50:32 +0300

    Exercise Data Types and Variables/7. Water Overflow/Program.cs solution added

---
+++ C# Fudamentals/Exercise Data Types and Variables/7. Water Overflow/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7.Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            const int WATERTANK = 255;
+            int waterLevel = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int quatityOfWater = int.Parse(Console.ReadLine());
+
+                if (waterLevel + quatityOfWater > WATERTANK)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                else
+                {
+                    waterLevel += quatityOfWater;
+                }
+            }
+            Console.WriteLine(waterLevel);
+
+
+
+
+        }
+    }
+}
