972fac082d8c93d0e40ce1e0ee23e6e46c25db5a
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-04 09:16:28 +0300

    Exercise Data Types and Variables/8. Beer Kegs/Program.cs solution added

---
+++ C# Fudamentals/Exercise Data Types and Variables/8. Beer Kegs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Beer_Kegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nLines = int.Parse(Console.ReadLine());
+
+            double biggestKeg = double.MinValue;
+            string biggesKegName = string.Empty;
+
+            for (int i = 0; i < nLines; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int hight = int.Parse(Console.ReadLine());
+
+                double volumeOfTheCurrentKeg = Math.PI * Math.Pow(radius, 2) * hight;
+
+                if (volumeOfTheCurrentKeg > biggestKeg)
+                {
+                    biggestKeg = volumeOfTheCurrentKeg;
+                    biggesKegName = model;
+
+                }
+            }
+
+            Console.WriteLine(biggesKegName);
+        }
+    }
+}
