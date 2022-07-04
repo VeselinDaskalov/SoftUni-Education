dc34aa219937a7ff922bbf9e257c8c8c213cfc12
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-04 09:18:47 +0300

    Exercise Data Types and Variables/10. Pokemon/Program.cs solution added

---
+++ C# Fudamentals/Exercise Data Types and Variables/10. Pokemon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhustionFactor = int.Parse(Console.ReadLine());
+            int startingPower = power;
+            int countOfPokedTargets = 0;
+
+            while (power >= distance)
+            {
+                power -= distance;
+                countOfPokedTargets++;
+                if (startingPower * 0.5 == power && exhustionFactor > 0)
+                {
+                    power /= exhustionFactor;
+                }
+            }
+
+            Console.WriteLine(power);
+            Console.WriteLine(countOfPokedTargets);
+
+        }
+    }
+}
