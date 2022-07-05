8728ed53ecb4339b31f2b9dc1396a14468909528
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-05 07:46:21 +0300

    Arrays/1.DayOfWeek/Program.cs solution added

---
+++ C# Fudamentals/Arrays/1.DayOfWeek/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.DayOfWeek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int dayNumber = int.Parse(Console.ReadLine());
+            string[] daysOfWeek = new string[]
+            {
+                "Monday",
+                "Tuesday",
+                "Wednesday",
+                "Thursday",
+                "Friday",
+                "Saturday",
+                "Sunday"
+            };
+
+            if (dayNumber >= 1 && dayNumber <= daysOfWeek.Length)
+            {
+                Console.WriteLine(daysOfWeek[dayNumber - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+
+            
+        }
+    }
+}
