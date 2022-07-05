7b7a82b1dcc7647d6f1d49513fcce64a3d8ca3d6
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-05 07:48:57 +0300

    Arrays/3RoundingNumbers/Program.cs solution added

---
+++ C# Fudamentals/Arrays/3RoundingNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3RoundingNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = Console.ReadLine().Split();
+            double[] items = new double[rawInput.Length];
+
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                items[i] = double.Parse(rawInput[i]);
+            }
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"{items[i]} => {Math.Round(items[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
