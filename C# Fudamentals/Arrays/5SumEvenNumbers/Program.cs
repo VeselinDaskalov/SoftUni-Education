b719bb256f9e20623226089c107bb7ea37de37ed
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-06 07:48:07 +0300

    Arrays/5SumEvenNumbers/Program.cs solution added

---
+++ C# Fudamentals/Arrays/5SumEvenNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5SumEvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] items = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+            for (int i = 0; i <items.Length; i++)
+            {
+                int currentValue = items[i];
+                if (currentValue % 2 == 0)
+                {
+                    sum += currentValue;
+                }
+            }
+            Console.WriteLine(sum);
+
+            //foreach(int item in items)
+            //{
+            //    if (item % 2 == 0)
+            //    {
+            //        sum += item;
+            //    }
+            //}
+        }
+    }
+}
