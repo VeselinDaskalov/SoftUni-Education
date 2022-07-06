c5571381e13aef85e90e1709341057caa2794405
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-06 07:46:54 +0300

    Arrays/4ReverseArrayOfStrings/Program.cs solution added

---
+++ C# Fudamentals/Arrays/4ReverseArrayOfStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4ReverseArrayOfStrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] items = Console.ReadLine().Split();
+
+            Array.Reverse(items);
+            Console.WriteLine(string.Join(" ", items));
+        }
+    }
+}
