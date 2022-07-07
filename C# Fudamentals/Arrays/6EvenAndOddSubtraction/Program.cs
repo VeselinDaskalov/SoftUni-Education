366534ff21a58403f4fd24da717cfd509ac89e3a
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-07 09:56:20 +0300

    Arrays/6EvenAndOddSubtraction/Program.cs solution added

---
+++ C# Fudamentals/Arrays/6EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6EvenAndOddSubtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = Console.ReadLine().Split();
+            int[] items = new int[rawInput.Length];
+
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                items[i] = int.Parse(rawInput[i]);
+            }
+            int evenSum = 0;
+            int oddSum = 0; 
+            foreach (int item in items)
+            {
+                if (item % 2 == 0)
+                {
+                    evenSum += item;
+                }
+                else
+                {
+                    oddSum += item;
+                }
+            }
+
+            Console.WriteLine(evenSum - oddSum);
+        }
+    }
+}
