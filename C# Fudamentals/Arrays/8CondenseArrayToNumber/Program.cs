5e4f0d5d663e7e0af3102566728f35ed5b387942
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-07 09:58:29 +0300

    Arrays/8CondenseArrayToNumber/Program.cs solution added

---
+++ C# Fudamentals/Arrays/8CondenseArrayToNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8CondenseArrayToNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int firstLength = array.Length;
+            
+            for (int i = 0; i < firstLength - 1; i++)
+            {
+                int[] condensed = new int[array.Length - 1];
+
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    condensed[j] = array[j] + array[j + 1];
+                }
+
+                array = condensed;
+            }
+
+            Console.WriteLine(array[0]);
+        }
+    }
+}
