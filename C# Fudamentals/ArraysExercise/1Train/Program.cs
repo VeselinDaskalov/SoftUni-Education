60753a96d4578c42935e3c91547449e0bdee114c
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-08 20:22:41 +0300

    ArraysExercise/1Train/Program.cs solution added

---
+++ C# Fudamentals/ArraysExercise/1Train/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfWagons = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            int[] peopleInWagon = new int[numberOfWagons];
+            for (int i = 0; i < numberOfWagons; i++)
+            {
+                peopleInWagon [i] = int.Parse(Console.ReadLine());
+
+                sum += peopleInWagon[i];
+            }
+            Console.WriteLine(String.Join(" ", peopleInWagon));
+            Console.WriteLine(sum);
+        }
+    }
+}
