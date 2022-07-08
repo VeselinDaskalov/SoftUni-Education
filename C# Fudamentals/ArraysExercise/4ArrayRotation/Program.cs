d7a7bcf441861227f7e30f9f7c2ab08e96f2b5ff
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-09 00:35:53 +0300

    ArraysExercise/4ArrayRotation/Program.cs solution added

---
+++ C# Fudamentals/ArraysExercise/4ArrayRotation/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(' ',(char)StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int rotationsCount = int.Parse(Console.ReadLine()); 
+            int rotationsCountReduce = rotationsCount % arr.Length;
+
+            for (int rot = 1; rot <= rotationsCountReduce; rot++)
+            {
+                int firstEl = arr[0];
+                for (int i = 0; i <= arr.Length - 2; i++)
+                {
+                    arr[i] = arr[i + 1];
+                }
+
+                arr[arr.Length - 1] = firstEl;
+            }
+
+            Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
