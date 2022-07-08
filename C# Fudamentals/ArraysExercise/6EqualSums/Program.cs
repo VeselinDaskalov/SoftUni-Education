e10b95f26b281d3cccdf7d6cb8186cf08b8749f5
Veselin Daskalov <daskalov_veselin@abv.bg>
2022-07-09 00:37:50 +0300

    ArraysExercise/6EqualSums/Program.cs solution added

---
+++ C# Fudamentals/ArraysExercise/6EqualSums/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6EqualSums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] digits = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            bool isFound = false;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                int leftSum = 0;
+                for (int k = 0; k < i; k++)
+                {
+                    leftSum += digits[k];
+                }
+
+                int rightSum = 0;
+                for (int j = digits.Length - 1; j > i; j--)
+                {
+                    rightSum += digits[j];
+                }
+
+                if (leftSum == rightSum && !isFound)
+                {
+                    Console.WriteLine(i);
+                    isFound = true;
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
